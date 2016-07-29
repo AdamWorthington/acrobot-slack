01786a07f4b9aa7308219aab825c1735d443cfb6
Connor Walsh <cwalsh@cwals1ml1.riotgames.com>
2016-07-29 14:20:04 -0700

    Fuck your yolo

--- src/basic.py
+++ src/basic.py
@@ -9,19 +9,26 @@ from slackclient import SlackClient
 token='x' +'oxb-64602165441-qTU3kuCfbVMCtRYHbeDUd5Zy'
 sc = SlackClient(token)
 
+def handleText(text):
+	if text == "yolo":
+        	sc.api_call(
+	 		"chat.postMessage", channel="#general", text="fuck your yolo",
+	 		username='pybot'
+	 		)
+
 def handle(message):
-	json_string=message
-	# obj = json.loads(json_string)
-	print(json_string)
-	# if obj == 'yolo':
-	# 	sc.api_call(
-	# 		"chat.postMessage", channel="#general", text="fuck your yolo",
-	# 		username='pybot'
-	# 		)
+	print("Handling message: " + str(message))
+	for key, value in message.iteritems():
+		print(key, value)
+		if key == "text":
+			handleText(message[key])
 
 if sc.rtm_connect():
+	print "Connection valid, reading from rtm"
 	while True:
-		handle(sc.rtm_read())
+		messages = sc.rtm_read()
+		for message in messages:
+			handle(message)
 		time.sleep(1)
 else:
 	print "Connection Failed, invalid token?"

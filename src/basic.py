06b6958236d16e341b2a4fd8776f2b0a2678109d
Adam Worthington <aworthington@riotgames.com>
2016-07-29 14:35:44 -0700

    yolo

--- src/basic.py
+++ src/basic.py
@@ -1,20 +1,24 @@
 import time
 import json
 import base64
+from acronym import Acronyms
 from slackclient import SlackClient
-
-
 
 
 token='x' +'oxb-64602165441-qTU3kuCfbVMCtRYHbeDUd5Zy'
 sc = SlackClient(token)
+bot = Acronyms()
+bot.put('yolo', 'you only live once')
 
 def handleText(text):
-	if text == "yolo":
-        	sc.api_call(
-	 		"chat.postMessage", channel="#general", text="fuck your yolo",
-	 		username='pybot'
-	 		)
+	x=bot.get(text)
+	if x == '':
+		return
+	
+	sc.api_call(
+		"chat.postMessage", channel="#general", text=x,
+		username='pybot'
+		)
 
 def handle(message):
 	for key, value in message.iteritems():
@@ -22,14 +26,25 @@ def handle(message):
 		if key == "text":
 			handleText(message[key])
 
-if sc.rtm_connect():
-	print "Connection valid, reading from rtm"
-	while True:
-		messages = sc.rtm_read()
-		for message in messages:
-			handle(message)
-		time.sleep(1)
-else:
-	print "Connection Failed, invalid token?"
 
 
+
+def main():
+	
+
+
+
+	if sc.rtm_connect():
+		print "Connection valid, reading from rtm"
+		while True:
+			messages = sc.rtm_read()
+			for message in messages:
+				handle(message)
+			time.sleep(1)
+	else:
+		print "Connection Failed, invalid token?"
+
+
+
+
+main()
